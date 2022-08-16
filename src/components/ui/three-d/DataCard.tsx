bd099dbf39db402bdd31aade1e0517bdd53d9232
KennyStanley <kpstanleyjr@gmail.com>
2022-08-16 19:32:20 +0000

    docs: added comments to the Babylon GUI React Component

--- src/components/ui/three-d/DataCard.tsx
+++ src/components/ui/three-d/DataCard.tsx
@@ -9,14 +9,18 @@ export const DataCard = () => {
   const textRef = useRef<TextBlock | null>(null)
 
   useEffect(() => {
+    // Get scene from machine and return if not there  
     const { scene } = state.context
     if (!scene) return
+
+    // After first render, just change the text
     if (textRef.current) {
       textRef.current.text = `state: ${state?.context?.data?.state}\n`
       textRef.current.text += `count: ${state?.context?.data?.count}`
       return
     }
 
+    // Create Plane for DataCard
     const dataCard = MeshBuilder.CreatePlane('dataCard', {
       height: 2,
       width: 2,
@@ -25,6 +29,7 @@ export const DataCard = () => {
 
     dataCard.position.y = 1.5
 
+    // BabylonJS GUI Element
     const advancedTexture = AdvancedDynamicTexture.CreateForMesh(
       dataCard,
       1024,
@@ -38,9 +43,9 @@ export const DataCard = () => {
     text.fontSize = 200
     advancedTexture.addControl(text)
 
+    // Set reference to TextBlock on initial render
     textRef.current = text
 
-    console.log('DataCard rendered')
   }, [state.context])
 
   return <></>
